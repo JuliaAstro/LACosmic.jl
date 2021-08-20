dcce3399ec90e3b323898c8135c1aa8f8c257676
Miles Lucas <mdlucas@hawaii.edu>
2021-08-19 23:20:16 -1000

    write up lax implementation (untested)

--- src/core.jl
+++ src/core.jl
@@ -1,14 +1,83 @@
+using ImageFiltering
+using Statistics
+using StaticArrays
 
-function lacosmic(data::AbstractMatrix, contrast=2.0, sigma_clip=4.5, objlim=2, niter=4, gain=nothing, readnoise=nothing)
-    block_size = 2
-    subimg = extend(subsample(data, block_size), StaticKernels.ExtensionSymmetric())
-    kernel = @kernel w -> max(0, w[0,-1] + w[-1,0] - 4*w[0,0] + w[1,0] + w[0,1])
-    conv_img = similar(data, size(kernel, subimg))
-    laplace_img = similar(data)
+function lacosmic(
+        data::AbstractMatrix;
+        mask=falses(size(data)),
+        contrast=2.0,
+        sigma_clip=4.5,
+        objlim=2,
+        maxiter=4,
+        gain=1,
+        background=0,
+        saturation_level=2^16
+        readnoise=nothing,
+        block_size=2)
+    # convert image to electrons using gain and background if previously subtracted
+    clean_image = muladd.(data, gain, background)
 
+    # update mask with saturated stars
+    mask = @. mask | (clean_image > saturation_level)
+    good_data = @view clean_image[.!mask]
+
+    # get the default background level for large cosmic rays
+    background_level = median(good_data)
+
+    # define cosmic ray mask
+    ray_mask = falses(size(data))
+
+
+    # loop for `maxiter` or if no more cosmic rays are found
     for iteration in 1:maxiter
-        map!(kernel, conv_img, subimg)
-        # block_reduce!(sum, laplace_img, conv_img)
-    end
+        # subsample, convolve, clip, and rebin
+        sub_img = subsample(clean_image, block_size)
+        conv_img = imfilter(sub_img, Kernel.Laplacian((false, false)), border="symmetric")
+        Lplus = rebin(conv_img)
 
+        # build Laplacian S/N map
+        medfilt = mapwindow(median, clean_image, (5, 5))
+        snr =  @. Lplus / (block_size * sqrt(medfilt + readnoise^2))
+        # remove large structions
+        snr_prime = snr .- mapwindow(median, snr, (5, 5))
+
+        # build fine structure image
+        f = mapwindow(median, clean_image, (3, 3))
+        back = mapwindow(median, clean_image, (7, 7))
+        f = @. (f - medfilt) / (block_size * sqrt(medfilt + readnoise^2))
+        
+        # find candidate cosmic rays
+        @. cosmics = !mask & snr_prime > sigma_clip & (snr_prime / f) > objlim
+
+        # determine neighborhood
+        cosmics = mapwindow(any, cosmics, (3, 3))
+        @. cosmics &= !mask & snr_prime > sigma_clip
+        cosmics = mapwindow(any, cosmics, (3, 3))
+        @. cosmics &= !mask & snr_prime > (sigma_clip * 0.3)
+        ray_mask .|= cosmics
+
+        @debug "iteration $iteration: found $(count(cosmics)) bad pixels this iteration"
+        
+        # if no more cosmics, break
+        if iszero(count(comsmics))
+            break
+        # otherwise, clean the image
+        else
+            clean_image = mapwindow(w -> max(mean(w), background_level), clean_image, (5, 5), indices=findall(ray_mask))
+        end
+    end
+    return clean_image, ray_mask
+end
+
+function rebin(arr)
+    dims = Int.(size(arr) ./ 2)
+    out = similar(arr, dims)
+    @inbounds for idx in CartesianIndices(out)
+        val = max(0, arr[idx.I[1] * 2 - 1, idx.I[2] * 2 - 1]) +
+              max(0, arr[idx.I[1] * 2 - 1, idx.I[2] * 2]) +
+              max(0, arr[idx.I[1] * 2, idx.I[2] * 2 - 1]) +
+              max(0, arr[idx.I[1] * 2, idx.I[2] * 2])
+        out[idx] = val
+    end
+    return out
 end
