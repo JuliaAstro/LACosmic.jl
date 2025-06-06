4cbd69326e59fff36d591968e1f7b5f1d1352e36
Ian Weaver <weaveric@gmail.com>
2025-06-06 16:21:46 -0700

    style

--- docs/make.jl
+++ docs/make.jl
@@ -7,22 +7,21 @@ DocMeta.setdocmeta!(LACosmic, :DocTestSetup, :(using LACosmic); recursive=true)
 include("pages.jl")
 
 makedocs(;
-    modules=[LACosmic],
-    authors="Miles Lucas <mdlucas@hawaii.edu> and contributors",
-    repo=GitHub("JuliaAstro/LACosmic.jl"),
-    sitename="LACosmic.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://juliaastro.org/LACosmic/stable/",
-        assets=String[],
+    modules = [LACosmic],
+    authors = "Miles Lucas <mdlucas@hawaii.edu> and contributors",
+    repo = GitHub("JuliaAstro/LACosmic.jl"),
+    sitename = "LACosmic.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://juliaastro.org/LACosmic/stable/",
     ),
-    pages=pages,
-    checkdocs=:exports,
+    pages = pages,
+    checkdocs = :exports,
 )
 
 deploydocs(;
-    repo="github.com/JuliaAstro/LACosmic.jl",
-    devbranch="main",
-    push_preview=true,
+    repo = "github.com/JuliaAstro/LACosmic.jl",
+    devbranch = "main",
+    push_preview = true,
     versions = ["stable" => "v^", "v#.#"], # Restrict to minor releases
 )
